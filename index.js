bd54abdd215e5e426c074d276c1c6ba544cd2db7
Kyle Mathews <mathews.kyle@gmail.com>
2014-10-06 10:33:46 -0700

    Really get working

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 var elementResizeEvent = require('element-resize-event');
 var throttle = require('lodash.throttle');
 
+var throttledSetState = throttle(function() {
+  this.setState({componentWidth: this.getDOMNode().offsetWidth});
+}, 16.666); // Throttle updates to 60 FPS.
+
 module.exports = {
   componentDidMount: function() {
     this.onResize();
     elementResizeEvent(this.getDOMNode(), this.onResize);
   },
-  onResize: function() { throttle(function() {
-      this.setState({componentWidth: this.getDOMNode().offsetWidth});
-    }, 16.666); // Throttle updates to 60 FPS.
-  }
+  onResize: throttledSetState
 };
